c8146c78c4ab4b86bb334044313ba4f6f6661cc3
Nguyen <nguyenguyen7595@gmail.com>
2023-01-16 14:37:29 -0500

    enable PWA

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { searchRobots, requestRobots } from './reducers';
 import { logger } from "redux-logger";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const store = configureStore({
@@ -31,3 +32,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
